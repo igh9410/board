24a9ec1a3c0caa78ff7773f54f471678d5cdf382
igh_9410 <31177070+Geon-Hyuk@users.noreply.github.com>
2022-06-09 16:19:36 +0900

    Added Login and Registration

---
+++ board/src/main/java/com/springboot/board/dao/UserRepository.java
@@ -0,0 +1,9 @@
+package com.springboot.board.dao;
+
+import com.springboot.board.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
---
+++ board/src/main/java/com/springboot/board/entity/User.java
@@ -0,0 +1,44 @@
+package com.springboot.board.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="user")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+    private Boolean enabled;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_role",
+            joinColumns = { @JoinColumn(name = "user_id") },
+            inverseJoinColumns = { @JoinColumn(name = "role_id") }
+    )
+    List<Role> roles = new ArrayList<>();
+
+
+
+
+
+
+}
